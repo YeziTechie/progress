2be4f0ef6538076c02f4960560d94474ffb12130
Yazdan <yazdan.hka@gmail.com>
2024-06-01 16:08:35 +0330

    fix - made the return data to dict

--- abilities/views/detail_ability.py
+++ abilities/views/detail_ability.py
@@ -1,6 +1,3 @@
-from typing import Any
-from django.db.models.base import Model as Model
-from django.db.models.query import QuerySet
 from django.views.generic import DetailView
 from abilities.models.ability import Ability
 from tasks.models.task import Task
@@ -11,7 +8,9 @@ class AbilityDetailView(DetailView):
     template_name = 'ability_detail.html'
 
     def get_context_data(self, **kwargs):
-        ability = super().get_context_data(**kwargs)
-        tasks = Task.objects.all(abiilty=ability)
+        context = super().get_context_data(**kwargs)
+        ability = context['ability']
+        tasks = Task.objects.filter(ability=ability)
+
         
-        return ability, tasks
+        return {'ability': ability, 'tasks': tasks}
