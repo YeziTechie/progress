e3cbfde1be3ea822eea73b682d744ccccfec1105
Yazdan <yazdan.hka@gmail.com>
2024-06-01 15:03:55 +0330

    feat - mapped ability detail view

--- abilities/urls.py
+++ abilities/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views.list_abilities import AbilityListGenericView
 from .views.create_ability import AbilityCreateView
 from .views.delete_ability import AbilityDeleteView
+from .views.detail_ability import AbilityDetailView
 
 urlpatterns = [
     path('', AbilityListGenericView.as_view(), name='ability_list'),
     path('list/', AbilityListGenericView.as_view(), name='ability_list'),
     path('create/', AbilityCreateView.as_view(), name='ability_create'),
     path('delete/<int:pk>/', AbilityDeleteView.as_view(), name='ability_delete'),
+    path('detail/<int:pk>/', AbilityDetailView.as_view(), name='ability_detail'),
 ]
