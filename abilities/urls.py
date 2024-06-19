abad22d32021175534855c488da8eeaeb2f2b1bd
Yazdan <yazdan.hka@gmail.com>
2024-06-20 00:57:13 +0330

    fix - changed the url names

--- abilities/urls.py
+++ abilities/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views.outcome_list import AbilityListGenericView
-from .views.create_outcome import AbilityCreateView
-from .views.delete_outcome import AbilityDeleteView
-from .views.outcome_detail import AbilityDetailView
+from .views.outcome_list import OutcomeListGenericView
+from .views.create_outcome import OutcomeCreateView
+from .views.delete_outcome import OutcomeDeleteView
+from .views.outcome_detail import OutcomeDetailView
 
 urlpatterns = [
-    path('', AbilityListGenericView.as_view(), name='ability_list'),
-    path('list/', AbilityListGenericView.as_view(), name='ability_list'),
-    path('create/', AbilityCreateView.as_view(), name='ability_create'),
-    path('delete/<int:pk>/', AbilityDeleteView.as_view(), name='ability_delete'),
-    path('detail/<int:pk>/', AbilityDetailView.as_view(), name='ability_detail'),
+    path('', OutcomeListGenericView.as_view(), name='outcome_list'),
+    path('list/', OutcomeListGenericView.as_view(), name='outcome_list'),
+    path('create/', OutcomeCreateView.as_view(), name='outcome_create'),
+    path('delete/<int:pk>/', OutcomeDeleteView.as_view(), name='outcome_delete'),
+    path('detail/<int:pk>/', OutcomeDetailView.as_view(), name='outcome_detail'),
 ]
