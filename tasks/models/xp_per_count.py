fb8f9e6818a369de3e76d1ae0accb4cd631eea0e
Yazdan <yazdan.hka@gmail.com>
2024-07-12 05:54:04 +0330

    feat(task per count): created model, defined rows in ideas

---
+++ tasks/models/xp_per_count.py
@@ -0,0 +1,27 @@
+from django.db import models
+from abilities.models.outcome import Outcome
+import datetime
+
+
+class XpPerCountTask(models.Model):
+
+    outcome = models.ForeignKey(
+        Outcome,
+        on_delete=models.SET_NULL,
+        related_name='xp_per_count_task_outcome',
+        null=True,
+        blank=True
+    )
+
+    description = models.CharField(max_length=256)
+
+    xp = models.IntegerField(default=0)
+    total_count = models.IntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_submit = models.DateTimeField(null=True, blank=True)
+
+
+
+    def __str__(self):
+        return f'{self.outcome} | {self.description} xp: {self.total_count}'
