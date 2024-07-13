3681e07fc708283c9428919a1639e608eda2f209
Yazdan <yazdan.hka@gmail.com>
2024-07-14 00:11:21 +0330

    urls from ability to outcome

--- _base/urls.py
+++ _base/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('abilities.urls')),
-    path('abilities/', include('abilities.urls')),
+    path('outcomes/', include('abilities.urls')),
     path('tasks/', include('tasks.urls')),
     path('user/', include('user.urls')),
 ]
