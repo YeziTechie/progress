8a4d725b86111e7771f7026387cabdca3c7b9d4b
Yazdan <yazdan.hka@gmail.com>
2024-07-15 03:13:46 +0330

    feat(model): adde dis added and is achievedx dto outcome

--- abilities/models/outcome.py
+++ abilities/models/outcome.py
@@ -6,6 +6,8 @@ class Outcome(models.Model):
     total_xp = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     last_task_done_at = models.DateTimeField(null=True, blank=True)
+    is_achieved = models.BooleanField(default=False)
+    is_hided = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.name} | XP: {self.total_xp}'
