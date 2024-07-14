1b5dc045660087870366ece5385c671f0652ca8f
Yazdan <yazdan.hka@gmail.com>
2024-07-15 02:44:13 +0330

    fix(view): changed the name of ocunt task

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,7 +6,7 @@ from tasks.views.classic_task.classic_task_delete import ClassicTaskDeleteView
 from tasks.views.classic_task.classic_task_detail import ClassicTaskDetailView
 from tasks.views.classic_task.classic_task_update import ClassicTaskUpdateView
 
-from tasks.views.count_task.create_xp_per_count_task import XpPerCountTaskCreateView
+from tasks.views.count_task.create_xp_per_count_task import CountTaskCreateView
 
 urlpatterns = [
     path('classic/list/', ClassicTaskListView.as_view(), name='classic_task_list'),
@@ -15,5 +15,5 @@ urlpatterns = [
     path('classic/detail/<int:pk>/', ClassicTaskDetailView.as_view(), name='classic_task_detail'),
     path('classic/update/<int:pk>/', ClassicTaskUpdateView.as_view(), name='classic_task_update'),
 
-    path('xp-per-count/create', XpPerCountTaskCreateView.as_view(), name='xp_per_count_task_create'),
+    path('xp-per-count/create', CountTaskCreateView.as_view(), name='count_task_create'),
 ]
