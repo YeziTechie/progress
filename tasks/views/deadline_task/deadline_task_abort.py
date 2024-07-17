d75928a59bf757e577797247689c9046074ccac3
Yazdan <yazdan.hka@gmail.com>
2024-07-17 16:29:44 +0330

    feat(tasks): added abort task half compelete. time is over

--- tasks/views/deadline_task/deadline_task_abort.py
+++ tasks/views/deadline_task/deadline_task_abort.py
@@ -14,7 +14,7 @@ class DeadlineTaskAbortView(FormView):
         task = get_object_or_404(DeadlineTask, pk=self.kwargs['pk'])
         task.is_aborted = True
         task.save()
-        return redirect('outcome_list')
+        return redirect('outcome_list'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
