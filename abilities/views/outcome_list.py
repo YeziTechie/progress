c304708eceafbdad437dfe1ca46cae229c780316
Yazdan <yazdan.hka@gmail.com>
2024-07-17 12:01:48 +0330

    feat(tasks): created deadline tasks

--- abilities/views/outcome_list.py
+++ abilities/views/outcome_list.py
@@ -1,5 +1,5 @@
 from django.views import generic
-
+from datetime import datetime
 from abilities.models.outcome import Outcome
 from tasks.models.classic_task import ClassicTask
 
@@ -12,6 +12,7 @@ class OutcomeListGenericView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         outcomes = context['outcomes']
+        print(datetime.now('tehran'))
 
         classic_tasks_without_outcome = ClassicTask.objects.filter(outcome__isnull=True)
 
---
+++ tasks/models/deadline_task.py
@@ -0,0 +1,45 @@
+from datetime import timezone, datetime
+
+from django.db import models
+
+
+class DeadLineTask(models.Model):
+
+    penalty_choices = [
+        'Half XP loose',
+        'Equal XP loose',
+        'Double XP loose',
+        'Four fold XP loose',
+    ]
+
+    outcome = models.ForeignKey(
+        'abilities.Outcome',
+        models.SET_NULL,
+        related_name='deadline_tasks',
+        null=True,
+        blank=True
+    )
+
+    description = models.CharField(max_length=512)
+    xp = models.IntegerField(default=0)
+    report = models.CharField(max_length=1024, null=True)
+    penalty = models.CharField(choices=penalty_choices, max_length=32)
+
+    deadline_date = models.DateTimeField(null=True, blank=True)
+    duration_days = models.IntegerField(null=True, blank=True)
+    duration_hours = models.IntegerField(null=True, blank=True)
+
+    is_done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.deadline_date:
+            if self.duration_days is not None or self.duration_hours is not None:
+                now = timezone.now()
+                days = self.duration_days if self.duration_days is not None else 0
+                hours = self.duration_hours if self.duration_hours is not None else 0
+                self.deadline_date = now + datetime.timedelta(days=days, hours=hours)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.description} (due {self.deadline_date})'
