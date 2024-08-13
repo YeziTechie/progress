7091d76e62f18ac10cacd00a4dfc70f5af12ac3c
Yazdan <yazdan.hka@gmail.com>
2024-08-14 00:27:47 +0330

    added create task button, increase the multiplier of level calculation

--- user/helpers/generate_level.py
+++ user/helpers/generate_level.py
@@ -1,10 +1,10 @@
-def calculate_level(xp, base_xp=100, xp_multiplier=1.2):
+def calculate_level(xp, base_xp=100, xp_multiplier=1.5):
     level = 0
     while xp >= calculate_xp_for_level(level + 1, base_xp, xp_multiplier):
         level += 1
     return level
 
 
-def calculate_xp_for_level(level, base_xp=100, xp_multiplier=1.2):
+def calculate_xp_for_level(level, base_xp=100, xp_multiplier=1.5):
     xp_required = base_xp * (xp_multiplier ** (level - 1))
     return round(xp_required)
