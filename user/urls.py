1bc00e3685c51a7dd9a9df08d28412ecf2bbac6d
YeziTechie <yazdan.hka@gmail.com>
2025-10-16 15:07:48 +0330

    feat: logout added

--- user/urls.py
+++ user/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from user.views.profile import UserProfileView
 from user.views.signup import signup_view
 from user.views.login import login_view
+from user.views.logout import logout_view
 
 
 urlpatterns = [
     path('', UserProfileView.as_view(), name='profile'),
     path('sign-up/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
