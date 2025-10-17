661919c97e9fff3a627de7a14f7718b8bfb0d2bd
YeziTechie <yazdan.hka@gmail.com>
2025-10-17 13:36:22 +0330

    navbar is done, working on little stuff

--- _base/settings.py
+++ _base/settings.py
@@ -124,6 +124,11 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+MEDIA_URL = '/media/'   # URL used to access media files
+MEDIA_ROOT = BASE_DIR / 'media'  # Folder where files will actually be stored
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
